5d8152795c8c151ec4bf1d94df58f197a37edc78
Claython-Remboski <claython.duarte@gmail.com>
2023-10-22 21:51:46 -0300

    Primeira versão do projeto em funcionamento. Versão 0.0.1

--- conversorDeMoedasPhp/runner.php
+++ conversorDeMoedasPhp/runner.php
@@ -26,7 +26,7 @@
         if (isset($_POST["valor"])) {
 
             //declaração da function Request
-            function request(string $urlApi, string $cookie)
+            function request(string $urlApi)
             {
 
                 //Iniciando a sessão curl
@@ -34,9 +34,7 @@
 
                 //Definindo os parâmetros
                 curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($curlSession, CURLOPT_HTTPHEADER, [
-                    'Cookie: ' . $cookie,
-                ]);
+
 
                 $data = curl_exec($curlSession);
 
@@ -60,58 +58,58 @@
             $dataInicial = date('m-d-Y');
             $dataFinalCotacao = date('m-d-Y');
 
-            $urlApi = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda='" . $moeda . "'&@dataInicial='" . $dataInicial . "'&@dataFinalCotacao='" . $dataFinalCotacao . "'&\$top=100&\$format=json&\$select=cotacaoCompra";
+            $urlApi = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda='" . $moeda . "'&@dataInicial='" . "10-20-2023" . "'&@dataFinalCotacao='" . $dataFinalCotacao . "'&\$top=100&\$format=json&\$select=cotacaoCompra";
 
 
-            define('cookie', 'TS01d9825e=012e4f88b356293b527c485f6879dccc579681ebbf114839d3297ad3e10c7d28b1830a47bf5be11442ffe8b9b8626e8f377063db76; dtCookie=26613C5AA100EBB4475AEEA5D1AF00C4|cHRheHwx; BIGipServer~was_p_as3~was_p~pool_was_443_p=4275048876.47873.0000; JSESSIONID=0000UGPUFrMf7vvUgDRnR7P1e9v:1dof85j5r; TS013694c2=012e4f88b3b796ad01ec3fb120e5f2921beb20f20ea272db276e54671d54f421701524c08b3c77402754631af866057a7139fd1e56');
-
-
-            $data = request($urlApi, cookie);
+            $data = request($urlApi);
 
 
             $dados = json_decode($data, true);
 
-            var_dump($dados);
-            
+
+
             if ($dados && isset($dados['value']) && !empty($dados['value'])) {
-                $primeiroItem = $dados['value'][0];
 
-                echo "Cotação de Compra: " . $primeiroItem['cotacaoCompra'] . "<br>";
+                $cotacaoCompra = $dados['value'][0];
+
+                echo "Cotação de Compra: " . $cotacaoCompra['cotacaoCompra'] . "<br>";
+
+
+                $valor = $_POST["valor"] ?? "sem_numero";
+                switch ($_POST["moeda"]) {
+                    case "USD":
+
+                        $USD = numfmt_create("us", NumberFormatter::CURRENCY);
+                        $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+                        $resultado = $valor / $cotacaoCompra['cotacaoCompra'];
+
+                        echo "Resultado: Seus  " . numfmt_format_currency($padrao, $valor, "BRL") . " equivalem a " . numfmt_format_currency($USD, $resultado, "USD");
+                        break;
+                    case "EUR":
+                        $EUR = numfmt_create("de_DE", NumberFormatter::CURRENCY);
+                        $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
+                        $resultado = $valor / $cotacaoCompra['cotacaoCompra'];
+                        echo "Resultado: Seus  " . numfmt_format_currency($padrao, $valor, "BRL") . " equivalem a " . numfmt_format_currency($EUR, $resultado, "EUR");
+                        break;
+                    case "JPY":
+                        $JPY = numfmt_create("ja_JP", NumberFormatter::CURRENCY);
+                        $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
+                        $resultado = $valor / $cotacaoCompra['cotacaoCompra'];
+                        echo "Resultado: Seus  " . numfmt_format_currency($padrao, $valor, "BRL") . " equivalem a " . numfmt_format_currency($JPY, $resultado, "JPY");
+                        break;
+
+                    default:
+                        echo "Nenhuma moeda selecionada";
+                        break;
+                }
             } else {
-                echo "Dados não recuperados corretamente.";
-            }
-
-
-            $valor = $_POST["valor"] ?? "sem_numero";
-            switch ($_POST["moeda"]) {
-                case "USD":
-
-                    $USD = numfmt_create("us", NumberFormatter::CURRENCY);
-                    $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
-                    $resultado = $valor / 5.06;
-
-                    echo "Resultado: Seus  " . numfmt_format_currency($padrao, $valor, "BRL") . " equivalem a " . numfmt_format_currency($USD, $resultado, "USD");
-                    break;
-                case "EUR":
-                    $EUR = numfmt_create("de_DE", NumberFormatter::CURRENCY);
-                    $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
-                    $resultado = $valor / 5.33;
-                    echo "Resultado: Seus  " . numfmt_format_currency($padrao, $valor, "BRL") . " equivalem a " . numfmt_format_currency($EUR, $resultado, "EUR");
-                    break;
-                case "YEN":
-                    $YEN = numfmt_create("ja_JP", NumberFormatter::CURRENCY);
-                    $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
-                    $resultado = $valor / 0.034;
-                    echo "Resultado: Seus  " . numfmt_format_currency($padrao, $valor, "BRL") . " equivalem a " . numfmt_format_currency($YEN, $resultado, "JPY");
-                    break;
-
-                default:
-                    echo "Nenhuma moeda selecionada";
-                    break;
+                echo "Nenhum valor digitado";
             }
         } else {
-            echo "Nenhum valor digitado";
+            echo "Dados de cotação não localizados neste momento.";
         }
+
+
 
 
         ?>
